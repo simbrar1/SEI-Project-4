f83818598b13e6cd6f967e0b5504a3ce7fa3bade
simbrar1 <sim.brar1@hotmail.com>
2019-07-31 14:20:19 +0100

    heroku test

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_uri = os.getenv('DATABASE_URI', 'postgres://localhost:5432/history')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/history')
 secret = os.getenv('SECRET', 'my secret')
--- config/router.py
+++ config/router.py
@@ -1,6 +1,17 @@
+import os
 from app import app
 from controllers import facts, auth, years
 
 app.register_blueprint(facts.api, url_prefix='/api')
 app.register_blueprint(years.api, url_prefix='/api')
 app.register_blueprint(auth.api, url_prefix='/api')
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
